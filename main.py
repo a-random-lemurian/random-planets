11400b3b173daf8c0f81f3329ca6aeec3fe38ca0
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-10-20 21:11:39 +0800

    no more legacy stars and planets generations!

--- main.py
+++ main.py
@@ -71,54 +71,6 @@ def stars(starstomake: int):
         print('Planet data written to disk.')
         
     print('DONE')
-    
-
-
-# Star systems (legacy)
-@app.command()
-def legacy_stars(stars: Optional[int] = typer.Argument(10)):
-    starid = CONFIG['stars-genned']
-    j = []
-    for i in range(stars):
-        name = f'{random.choice(STAR_SURVEYS)} {starid}'
-        x = {}
-        x['planets']  = []
-        x['name']     = name
-        x['spectral'] = random.choice(CONFIG['star-types'])
-        starid += 1
-        planet_num = 1
-        for i in range(random.randint(4,16)):
-            planet_list = []
-            planet_list.append(pcal.generatePlanet)
-        j.append(x)
-    with open(f'star-queries/{random.randint(1,100000)}.json','x') as f:
-        json.dump(j,f)
-
-
-
-
-# Generate random planets (legacy)
-@app.command()
-def legacy_planets(planets: Optional[int] = typer.Argument(500)):
-    x = {}
-    planetList = []
-    for i in range(planets):
-        x = {}
-        num = random.randint(1,9999999999999999999)
-        pop = random.randint(499500000,187500000000)
-        govt            = random.choice(GOVERNMENTS)
-        type_of_planet  = random.choice(PLANET_TYPES)
-        planet_diameter = random.randint(14000,55000)
-        #rich.print(f'[green][b]>[/b][/green] {random.choice(PLANET_SURVEYS)} {num: >12} {type_of_planet: >12} {pop: >16} {govt: >12}')
-        x['name'] = f'LACOP.{random.randint(1,1000)}.{random.randint(1,999999999999999999)}.{secrets.token_hex(8)}'
-        x['govt'] = govt
-        x['planetType'] = type_of_planet
-        x['population'] = pop
-        planetList.append(x)
-    json_query_file = f'stars-{secrets.token_urlsafe(16)}.json'
-    with open(json_query_file,'x') as fileSave:
-        rich.print('done')
-        json.dump(planetList,fileSave)
 
 # Manual
 @app.command()
